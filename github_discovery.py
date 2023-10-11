5eac4cd8dc8ffd9138f777f5fb6a7bce93298344
Matt <34448412+mattops@users.noreply.github.com>
2023-10-11 12:10:25 +0100

    Init project (#1)

---
+++ github_discovery.py
@@ -0,0 +1,482 @@
+#!/usr/bin/env python
+'''Github discovery - queries the github API for info about hmpps services and stores the results in the service catalogue'''
+import os
+import http.server
+import socketserver
+import threading
+import logging
+import tempfile
+from time import sleep
+from datetime import datetime
+from base64 import b64decode
+import re
+import json
+
+import yaml
+import github
+import requests
+from dockerfile_parse import DockerfileParser
+
+SC_API_ENDPOINT = os.getenv("SERVICE_CATALOGUE_API_ENDPOINT")
+SC_API_TOKEN = os.getenv("SERVICE_CATALOGUE_API_KEY")
+GITHUB_APP_ID = int(os.getenv("GITHUB_APP_ID"))
+GITHUB_APP_INSTALLATION_ID = int(os.getenv("GITHUB_APP_INSTALLATION_ID"))
+GITHUB_APP_PRIVATE_KEY = os.getenv("GITHUB_APP_PRIVATE_KEY")
+REFRESH_INTERVAL_HOURS = int(os.getenv("REFRESH_INTERVAL_HOURS", "6"))
+# Set maximum number of concurrent threads to run, try to avoid secondary github api limits.
+MAX_THREADS = 10
+LOG_LEVEL = os.environ.get('LOG_LEVEL', 'INFO').upper()
+
+# limit results for testing/dev
+#SC_FILTER = '&filters[name][$contains]=community-api'
+SC_FILTER = ''
+SC_PAGE_SIZE=10
+SC_PAGINATION_PAGE_SIZE=f"&pagination[pageSize]={SC_PAGE_SIZE}"
+# Example Sort filter
+#SC_SORT='&sort=updatedAt:asc'
+SC_SORT = ''
+SC_ENDPOINT = f"{SC_API_ENDPOINT}/v1/components?populate=environments{SC_FILTER}{SC_PAGINATION_PAGE_SIZE}{SC_SORT}"
+
+class HealthHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
+  def do_GET(self):
+    self.send_response(200)
+    self.send_header("Content-type", "text/plain")
+    self.end_headers()
+    self.wfile.write(bytes("UP", "utf8"))
+    return
+
+def update_sc_component(data, c_id):
+  try:
+    log.debug(data)
+    x = requests.put(f"{SC_API_ENDPOINT}/v1/components/{c_id}", headers=sc_api_headers, json = data, timeout=10)
+    if x.status_code == 200:
+      log.info(f"Successfully updated component id {c_id}: {x.status_code}")
+    else:
+      log.info(f"Received non-200 response from service catalogue for component id {c_id}: {x.status_code} {x.content}")
+  except Exception as e:
+    log.error(f"Error updating component in the SC: {e}")
+
+def get_file_yaml(repo, path):
+  try:
+    file_contents = repo.get_contents(path)
+    contents = b64decode(file_contents.content).decode().replace("\t", "  ")
+    yaml_contents = yaml.safe_load(contents)
+    return yaml_contents
+  except github.UnknownObjectException:
+    log.debug(f"404 File not found {repo.name}:{path}")
+  except Exception as e:
+    log.error(f"Error getting yaml file: {e}")
+
+def get_file_json(repo, path):
+  try:
+    file_contents = repo.get_contents(path)
+    json_contents = json.loads(b64decode(file_contents.content))
+    return json_contents
+  except github.UnknownObjectException:
+    log.debug(f"404 File not found {repo.name}:{path}")
+  except Exception as e:
+    log.error(f"Error getting json file: {e}")
+
+def get_file_plain(repo, path):
+  try:
+    file_contents = repo.get_contents(path)
+    plain_contents = b64decode(file_contents.content).decode()
+    return plain_contents
+  except github.UnknownObjectException:
+    log.debug(f"404 File not found {repo.name}:{path}")
+    return False
+  except Exception as e:
+    log.error(f"Error getting contents from file: {e}")
+
+def test_endpoint(url, endpoint):
+  headers = {'User-Agent': 'hmpps-service-discovery'}
+  try:
+    r = requests.get(f"{url}{endpoint}", headers=headers, allow_redirects=False, timeout=10)
+    # Test if json is returned
+    if r.json():
+      log.debug(f"Found endpoint: {url}{endpoint} ")
+      return True
+  except Exception:
+    log.debug(f"Couldn't connect to endpoint: {url}{endpoint} ")
+    return False
+
+def process_repo(c_name, c_id):
+  log.info(c_name)
+  try:
+    repo = gh.get_repo(f"ministryofjustice/{c_name}")
+    default_branch = repo.get_branch(repo.default_branch)
+    branch_protection = default_branch.get_protection()
+  except Exception as e:
+    log.error(f"Error with ministryofjustice/{c_name}, check github app has permissions to see it. {e}")
+    return False
+
+  # Empty data dict gets populated along the way, and finally used in PUT request to service catalogue
+  data = {}
+
+  # Add standard github repo properties
+  data.update({"language": repo.language})
+  data.update({"description": repo.description})
+  data.update({"github_project_visibility": repo.visibility})
+  data.update({"github_repo": repo.name})
+
+  # GitHub teams access, branch protection etc.
+  branch_protection_restricted_teams = []
+  teams_write = []
+  teams_admin = []
+  teams_maintain = []
+
+  try:
+    branch_protection = default_branch.get_protection()
+    branch_protection_teams = branch_protection.get_team_push_restrictions() or []
+    for team in branch_protection_teams:
+      branch_protection_restricted_teams.append(team.slug)
+  except Exception as e:
+    log.error(f"Unable to get branch protection {repo.name}: {e}")
+
+  teams = repo.get_teams()
+  for team in teams:
+    team_permissions = team.get_repo_permission(repo)
+    if team_permissions.admin:
+      teams_admin.append(team.slug)
+    elif team_permissions.maintain:
+      teams_maintain.append(team.slug)
+    elif team_permissions.push:
+      teams_write.append(team.slug)
+
+  data.update({"github_project_teams_admin": teams_admin})
+  log.debug(f"teams_admin: {teams_admin}")
+
+  data.update({"github_project_teams_maintain": teams_maintain})
+  log.debug(f"teams_maintain: {teams_maintain}")
+
+  data.update({"github_project_teams_write": teams_write})
+  log.debug(f"teams_write: {teams_write}")
+
+  data.update({"github_project_branch_protection_restricted_teams": branch_protection_restricted_teams})
+  log.debug(f"branch_protection_restricted_teams: {branch_protection_restricted_teams}")
+
+  # Github topics
+  topics = repo.get_topics()
+  data.update({"github_topics": topics})
+
+  versions_data = {}
+  # CircleCI config
+  cirlcleci_config = get_file_yaml(repo, ".circleci/config.yml")
+  if cirlcleci_config:
+    try:
+      cirleci_orbs = cirlcleci_config['orbs']
+      for key, value in cirleci_orbs.items():
+        if "ministryofjustice/hmpps" in value:
+          hmpps_orb_version=value.split('@')[1]
+          versions_data.update({'circleci': {'orbs': {'hmpps': hmpps_orb_version}}})
+          log.debug(f"hmpps orb version: {hmpps_orb_version}")
+    except Exception:
+      log.debug('No hmpps orb version found')
+
+  # Helm charts
+  helm_chart = get_file_yaml(repo, f"helm_deploy/{repo.name}/Chart.yaml") or {}
+  if 'dependencies' in helm_chart:
+    helm_dep_versions = {}
+    for item in helm_chart['dependencies']:
+      helm_dep_versions.update({item['name']: item['version']})
+    versions_data.update({'helm': {'dependencies': helm_dep_versions}})
+
+  helm_environments = []
+  try:
+    helm_deploy = repo.get_contents("helm_deploy", default_branch.commit.sha)
+  except Exception as e:
+    helm_deploy = False
+    log.debug(f"helm_deploy folder: {e}")
+
+  if helm_deploy:
+    for file in helm_deploy:
+      if file.name.startswith('values-'):
+        env = re.match('values-([a-z0-9-]+)\\.y[a]?ml', file.name)[1]
+        helm_environments.append(env)
+    helm_default_values = get_file_yaml(repo, f"helm_deploy/{repo.name}/values.yaml")
+    if helm_default_values:
+      # Try to get the container image
+      try:
+        container_image = helm_default_values['image']['repository']
+        data.update({"container_image": container_image})
+      except KeyError:
+        pass
+      try:
+        container_image = helm_default_values['generic-service']['image']['repository']
+        data.update({"container_image": container_image})
+      except KeyError:
+        pass
+
+  # helm env values files, extract useful values
+  helm_envs={}
+  for env in helm_environments:
+    values = get_file_yaml(repo, f"helm_deploy/values-{env}.yaml")
+    if values:
+      # Ingress hostname
+      try:
+        host = values['generic-service']['ingress']['host']
+        helm_envs.update({env: {'host': host}})
+        log.debug(f"{env} ingress host: {host}")
+      except KeyError:
+        pass
+      # Ingress alternative location
+      try:
+        host = values['generic-service']['ingress']['hosts'][0]
+        helm_envs.update({env: {'host': host}})
+        log.debug(f"{env} ingress host: {host}")
+      except KeyError:
+        pass
+      # Container image alternative location
+      try:
+        container_image = values['image']['repository']
+        data.update({"container_image": container_image})
+      except KeyError:
+        pass
+      try:
+        container_image = values['generic-service']['image']['repository']
+        data.update({"container_image": container_image})
+      except KeyError:
+        pass
+
+  environments = []
+  if repo.name in bootstrap_projects:
+    p = bootstrap_projects[repo.name]
+    # Get dev namespace data
+    if 'circleci_project_k8s_namespace' in p:
+      dev_namespace = p['circleci_project_k8s_namespace']
+      e={'namespace': dev_namespace, 'type': 'dev'}
+      if 'dev' in helm_envs:
+        dev_url = f"https://{helm_envs['dev']['host']}"
+        e.update({'name': 'dev', 'url': dev_url})
+      elif 'development' in helm_envs:
+        dev_url = f"https://{helm_envs['development']['host']}"
+        e.update({'name': 'development', 'url': dev_url})
+      else:
+        dev_url = False
+
+      if dev_url:
+        if test_endpoint(dev_url, '/health'):
+          e.update({'health_path': '/health'})
+        if test_endpoint(dev_url, '/info'):
+          e.update({'info_path': '/info'})
+        # Hack for hmpps-auth non standard endpoints
+        if 'sign-in' in dev_url and test_endpoint(dev_url, '/auth/info'):
+          e.update({'info_path': '/auth/info'})
+        if 'sign-in' in dev_url and test_endpoint(dev_url, '/auth/health'):
+          e.update({'health_path': '/auth/health'})
+      environments.append(e)
+    # Get other env namespaces based on circleci context data
+    if 'circleci_context_k8s_namespaces' in p:
+      for c in p['circleci_context_k8s_namespaces']:
+        e = {}
+        env_name=c['env']
+        # Try to set env type according to name e.g. stage or staging, prod or production
+        if env_name.startswith('dev'):
+          env_type='dev'
+        elif env_name.startswith('test'):
+          env_type='test'
+        elif env_name.startswith('stag'):
+          env_type='stage'
+        elif env_name.startswith('preprod'):
+          env_type='preprod'
+        elif env_name.startswith('prod'):
+          env_type='prod'
+        e.update({'type': env_type})
+
+        if env_name in helm_envs:
+          env_url=f"https://{helm_envs[env_name]['host']}"
+          e.update({'name': env_name, 'url': env_url})
+        elif 'developement' in helm_envs:
+          env_url=f"https://{helm_envs['developement']['host']}"
+          e.update({'type': 'dev', 'name': 'developement', 'url': env_url})
+        elif 'test' in helm_envs:
+          env_url=f"https://{helm_envs['test']['host']}"
+          e.update({'type': 'test', 'name': 'test', 'url': env_url})
+        elif 'testing' in helm_envs:
+          env_url=f"https://{helm_envs['testing']['host']}"
+          e.update({'type': 'test', 'name': 'testing', 'url': env_url})
+        elif 'staging' in helm_envs:
+          env_url=f"https://{helm_envs['staging']['host']}"
+          e.update({'type': 'stage', 'name': 'staging', 'url': env_url})
+        elif 'production' in helm_envs:
+          env_url=f"https://{helm_envs['production']['host']}"
+          e.update({'type': 'prod', 'name': 'production', 'url': env_url})
+        else:
+          env_url = False
+
+        if 'namespace' in c:
+          env_namespace=c['namespace']
+        else:
+          env_namespace=f"{repo.name}-{env_name}"
+        e.update({'namespace': env_namespace})
+
+        if env_url:
+          if test_endpoint(env_url, '/health'):
+            e.update({'health_path': '/health'})
+          if test_endpoint(env_url, '/info'):
+            e.update({'info_path': '/info'})
+          # Hack for hmpps-auth non standard endpoints
+          if 'sign-in' in env_url and test_endpoint(dev_url, '/auth/info'):
+            e.update({'info_path': '/auth/info'})
+          if 'sign-in' in env_url and test_endpoint(dev_url, '/auth/health'):
+            e.update({'health_path': '/auth/health'})
+
+        environments.append(e)
+
+  # App insights cloud_RoleName
+  if repo.language == 'Kotlin' or repo.language == 'Java':
+    app_insights_config = get_file_json(repo, 'applicationinsights.json')
+    if app_insights_config:
+      app_insights_cloud_role_name = app_insights_config['role']['name']
+      data.update({"app_insights_cloud_role_name": app_insights_cloud_role_name})
+
+  if repo.language == 'JavaScript' or repo.language == 'TypeScript':
+    package_json = get_file_json(repo, 'package.json')
+    if package_json:
+      app_insights_cloud_role_name = package_json['name']
+      if re.match(r'^[a-zA-Z0-9-_]+$', app_insights_cloud_role_name):
+        data.update({"app_insights_cloud_role_name": app_insights_cloud_role_name})
+
+  # Gradle config
+  build_gradle_config_content = False
+  if repo.language == 'Kotlin' or repo.language == 'Java':
+    build_gradle_kts_config = get_file_plain(repo, 'build.gradle.kts')
+    build_gradle_config_content = build_gradle_kts_config
+  # Try alternative location for java projects
+  if not build_gradle_config_content:
+    build_gradle_java_config = get_file_plain(repo, 'build.gradle')
+    build_gradle_config_content = build_gradle_java_config
+
+  if build_gradle_config_content:
+    try:
+      regex = 'id\\(\\"uk.gov.justice.hmpps.gradle-spring-boot\\"\\) version \\"(.*)\\"( apply false)?$'
+      hmpps_gradle_spring_boot_version = re.search(regex, build_gradle_config_content, re.MULTILINE)[1]
+      log.debug(f"Found hmpps gradle-spring-boot version: {hmpps_gradle_spring_boot_version}")
+      versions_data.update({'gradle': {'hmpps_gradle_spring_boot': hmpps_gradle_spring_boot_version}})
+    except TypeError:
+      pass
+
+  # Parse Dockerfile
+  try:
+    file_contents = repo.get_contents('Dockerfile')
+    dockerfile = DockerfileParser(fileobj=tempfile.NamedTemporaryFile())
+    dockerfile.content = b64decode(file_contents.content)
+    # Get list of parent images, and strip out references to 'base'
+    parent_images = list(filter(lambda i: i != 'base', dockerfile.parent_images))
+    # Get the last element in the array, which should be the base image of the final stage.
+    base_image = parent_images[-1]
+    versions_data.update({'dockerfile': {'base_image': base_image}})
+    log.debug(f"Found Dockerfile base image: {base_image}")
+  except github.UnknownObjectException:
+    log.info(f"404 File not found {repo.name}:Dockerfile")
+  except Exception as e:
+    log.error(f"Error parsing Dockerfile: {e}")
+
+  # Add Environments to final data dict
+  data.update({"environments": environments})
+
+  # Add versions to final data dict.
+  data.update({'versions': versions_data})
+
+  # Update component with all results in data dict.
+  update_sc_component({"data": data}, c_id)
+
+def startHttpServer():
+  handler_object = HealthHttpRequestHandler
+  with socketserver.TCPServer(("", 8080), handler_object) as httpd:
+    httpd.serve_forever()
+
+def process_components(data):
+  log.info(f"Processing batch of {len(data)} components...")
+  for component in data:
+    # Wait until the API limit is reset if we are close to the limit
+    while core_rate_limit.remaining < 100:
+      time_delta = datetime.now() - core_rate_limit.reset
+      time_to_reset = time_delta.total_seconds()
+      log.info(f"Github API rate limit {core_rate_limit}")
+      log.info(f"Backing off for {time_to_reset} second, to avoid github API limits.")
+      sleep(time_to_reset)
+
+    c_name = component["attributes"]["name"]
+    c_id = component["id"]
+    t_repo = threading.Thread(target=process_repo, args=(c_name,c_id), daemon=True)
+
+    # Apply limit on total active threads, avoid github secondary API rate limit
+    while threading.active_count() > (MAX_THREADS-1):
+      log.debug(f"Active Threads={threading.active_count()}, Max Threads={MAX_THREADS}")
+      sleep(10)
+    t_repo.start()
+    log.info(f"Started thread for {c_name}")
+
+if __name__ == '__main__':
+  logging.basicConfig(
+      format='[%(asctime)s] %(levelname)s %(threadName)s %(message)s', level=LOG_LEVEL)
+  log = logging.getLogger(__name__)
+
+  sc_api_headers = {"Authorization": f"Bearer {SC_API_TOKEN}", "Content-Type":"application/json","Accept": "application/json"}
+
+  # Test connection to Service Catalogue
+  try:
+    r = requests.head(f"{SC_API_ENDPOINT}/_health", headers=sc_api_headers, timeout=10)
+    log.info(f"Successfully connected to the Service Catalogue. {r.status_code}")
+  except Exception as e:
+    log.critical("Unable to connect to the Service Catalogue.")
+    raise SystemExit(e) from e
+
+  # Test auth and connection to github
+  try:
+    private_key = b64decode(GITHUB_APP_PRIVATE_KEY).decode('ascii')
+    auth = github.Auth.AppAuth(GITHUB_APP_ID, private_key).get_installation_auth(GITHUB_APP_INSTALLATION_ID)
+    gh = github.Github(auth=auth,pool_size=50)
+
+    rate_limit = gh.get_rate_limit()
+    core_rate_limit = rate_limit.core
+    log.info(f"Github API: {rate_limit}")
+    # test fetching organisation name
+    gh.get_organization("ministryofjustice")
+  except Exception as e:
+    log.critical("Unable to connect to the github API.")
+    raise SystemExit(e) from e
+
+  # Get projects.json from bootstrap repo for namespaces data
+  bootstrap_repo = gh.get_repo("ministryofjustice/dps-project-bootstrap")
+  bootstrap_projects_json = get_file_json(bootstrap_repo, 'projects.json')
+  # Convert dict for easier lookup
+  bootstrap_projects={}
+  for p in bootstrap_projects_json:
+    bootstrap_projects.update({p['github_repo_name']: p})
+
+  while True:
+    # Start health endpoint.
+    httpHealth = threading.Thread(target=startHttpServer, daemon=True)
+    httpHealth.start()
+
+    log.info(SC_ENDPOINT)
+    try:
+      r = requests.get(SC_ENDPOINT, headers=sc_api_headers, timeout=10)
+      log.debug(r)
+      if r.status_code == 200:
+        j_meta = r.json()["meta"]["pagination"]
+        log.debug(f"Got result page: {j_meta['page']} from SC")
+        j_data = r.json()["data"]
+        process_components(j_data)
+      else:
+        raise Exception(f"Received non-200 response from Service Catalogue: {r.status_code}")
+
+      # Loop over the remaining pages and return one at a time
+      num_pages = j_meta['pageCount']
+      for p in range(2, num_pages+1):
+        page=f"&pagination[page]={p}"
+        r = requests.get(f"{SC_ENDPOINT}{page}", headers=sc_api_headers, timeout=10)
+        if r.status_code == 200:
+          j_meta = r.json()["meta"]["pagination"]
+          log.debug(f"Got result page: {j_meta['page']} from SC")
+          j_data = r.json()["data"]
+          process_components(j_data)
+        else:
+          raise Exception(f"Received non-200 response from Service Catalogue: {r.status_code}")
+
+    except Exception as e:
+      log.error(f"Problem with Service Catalogue API. {e}")
+
+    sleep((REFRESH_INTERVAL_HOURS * 60 * 60))

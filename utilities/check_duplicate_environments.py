e0ff87e14b956b75dc3eb8dafd5dca0c146e4565
s99mj <sandhya.gandalwar@justice.gov.uk>
2026-03-06 13:03:55 +0000

    Updated to exclude any environment which are not linked to SC component

--- utilities/check_duplicate_environments.py
+++ utilities/check_duplicate_environments.py
@@ -60,52 +60,82 @@ def main():
   environments = sc.get_all_records('environments?populate=component')
   env_dict = {}
   for env in environments:
-    #   attrs = env.get('attributes', {})
     env_name = env.get('name')
     component_name = (env.get('component') or {}).get('name')
+    namespace = env.get('namespace')
+    document_id = env.get('documentId') or env.get('id')
+
     if env_name in env_dict:
       if component_name in env_dict.get(env_name):
-        env_dict[env_name][component_name] += 1
-      else:
-        env_dict[env_name][component_name] = 1
-    else:
-      env_dict[env_name] = {component_name: 1}
-
-  for env, component in env_dict.items():
-    for component_name, qty in component.items():
-      if qty > 1:
-        if component_name:
-          sc_component_filter = (
-            f'&filters[$and][0][component][name][$eq]={component_name}'
+        if namespace in env_dict[env_name][component_name]:
+          env_dict[env_name][component_name][namespace]['qty'] += 1
+          env_dict[env_name][component_name][namespace]['document_ids'].append(
+            document_id
           )
         else:
-          sc_component_filter = '&filters[$and][0][component][name][$null]=true'
-        if env:
-          sc_env_filter = f'&filters[$and][1][name][$eq]={env}'
-        else:
-          sc_env_filter = '&filters[$and][1][name][$null]=true'
-        link_url = f'{sc_link_stub}{sc_component_filter}{sc_env_filter}'
-        if not message:
-          message += '*Environments*:\n===========\n'
-        message += f'- <{link_url}|{env} - {component_name}> has {qty} entries\n'
+          env_dict[env_name][component_name][namespace] = {
+            'qty': 1,
+            'document_ids': [document_id],
+          }
+      else:
+        env_dict[env_name][component_name] = {
+          namespace: {
+            'qty': 1,
+            'document_ids': [document_id],
+          }
+        }
+    else:
+      env_dict[env_name] = {
+        component_name: {
+          namespace: {
+            'qty': 1,
+            'document_ids': [document_id],
+          }
+        }
+      }
 
-        # Construct the message
-        if message:
-          slack_template['blocks'][1]['text']['text'] = message
-          with open('slack-message.json', 'w') as f:
-            json.dump(slack_template, f)
-          f.close()
-          value = 'YES'
-        else:
-          value = 'NO'
+  for env, component in env_dict.items():
+    for component_name, namespace_data in component.items():
+      for namespace, env_info in namespace_data.items():
+        qty = env_info['qty']
+        if qty > 1:
+          if component_name:
+            sc_component_filter = (
+              f'&filters[$and][0][component][name][$eq]={component_name}'
+            )
+          else:
+            sc_component_filter = '&filters[$and][0][component][name][$null]=true'
+          if env:
+            sc_env_filter = f'&filters[$and][1][name][$eq]={env}'
+          else:
+            sc_env_filter = '&filters[$and][1][name][$null]=true'
+          link_url = f'{sc_link_stub}{sc_component_filter}{sc_env_filter}'
 
-        gh_out = os.environ.get('GITHUB_OUTPUT')
-        if gh_out:
-          with open(gh_out, 'a') as fh:
-            fh.write(f'results={value}\n')
-          eprint(f'Wrote to $GITHUB_OUTPUT: results={value}')
-        else:
-          eprint('GITHUB_OUTPUT env var not found')
+          doc_id_list = ', '.join([str(doc_id) for doc_id in env_info['document_ids']])
+          if not message:
+            message += '*Environments*:\n===========\n'
+          message += (
+            f'- <{link_url}|{env} - {component_name}> has {qty} entries '
+            f'(namespace: {namespace}, documentId: {doc_id_list})\n'
+          )
+
+          # Construct the message
+          if message:
+            slack_template['blocks'][1]['text']['text'] = message
+            with open('slack-message.json', 'w') as f:
+              json.dump(slack_template, f)
+            f.close()
+            value = 'YES'
+          else:
+            value = 'NO'
+
+          gh_out = os.environ.get('GITHUB_OUTPUT')
+          if gh_out:
+            with open(gh_out, 'a') as fh:
+              fh.write(f'results={value}\n')
+            eprint(f'Wrote to $GITHUB_OUTPUT: results={value}')
+          else:
+            eprint('GITHUB_OUTPUT env var not found')
 
 
 if __name__ == '__main__':
